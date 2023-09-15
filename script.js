5a25a6332673958a0def0e8ba87e4896a2c568a7
Syed zain ali shah <zaingit780@gmail.com>
2023-09-15 11:35:37 -0700

    add all file

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "🎉Correct Answer!";
+document.querySelector(".number").textContent = 20;
+document.querySelector(".score").textContent = 10;
+document.querySelector(".guess").value = 10;
+
+function hanfleDaa(){
+    
+}
