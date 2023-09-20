1d951b3f586d99b64276f9dc08d01f18e8606517
Syed zain ali shah <zaingit780@gmail.com>
2023-09-20 11:46:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-// console.log(document.querySelector(".message").textContent);
-// document.querySelector(".message").textContent = "🎉Correct Answer!";
-// document.querySelector(".number").textContent = 20;
-// document.querySelector(".score").textContent = 10;
-// document.querySelector(".guess").value = 10;
-
-// function hanfleDaa(){
-
-// }
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 document.querySelector(".score").textContent = score;
-document.querySelector(".number").textContent = secretNumber;
+
+console.log("secretNumber", secretNumber);
+console.log("score", score);
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log("guess", guess);
   if (!guess) {
     document.querySelector(".message").textContent = "🤔 No Number";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🤑 Correct Answer";
+    document.body.style.backgroundColor = "green";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector(".number").style.width = "30rem";
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "🤒 Too High";
@@ -34,8 +31,15 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "😌 You lost the game";
       document.querySelector(".score").textContent = 0;
     }
-  } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "🤑 Correct Answer";
-    document.body.style.backgroundColor = "green";
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start guessing";
+  document.querySelector(".guess").value = "";
+  document.body.style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
